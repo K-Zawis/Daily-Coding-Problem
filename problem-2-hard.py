bd9ec53ca55bd36cc9038b51c9cd9d9bcf0c2d5b
K-Zawis <zawistowska.kasia@outlook.com>
2023-08-04 18:48:28 +0100

    added some comments

--- problem-2-hard.py
+++ problem-2-hard.py
@@ -10,18 +10,40 @@ would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1],
 the expected output would be [2, 3, 6].
 """
 
-def prod(num_list):
+"""
+params
+    num_list - List[int]
+
+Takes an array of integers and calulates the priduct of all numbers
+insdie of the array.
+
+returns int
+"""
+def prod(num_list) -> int:
     product = 1
     for num in num_list:
         product *= num
     return product
 
-
+"""
+global variables
+"""
 num_list = [3, 2, 1]
 
 def list_of_elem_prods(num_list):
+    """
+    we begin by calling the prod function to retrieve the product of 
+    all elements in num_list
+    """
     product = prod(num_list)
 
+    """
+    for each number in our num_list we map the values to the product 
+    divided by number using List Comprehension:
+
+        [ expression for element in list_to_map]
+
+    """
     return [product // num for num in num_list]
 
 print(list_of_elem_prods(num_list))
@@ -40,10 +62,14 @@ def follow_up(num_list):
         """
         temp = num_list[:]
         temp.pop(index)
-        # calculate product
+        """
+        calculate product by calling our prod function again
+        """
         product = prod(temp)
 
-        # append product
+        """
+        append each product to our new list
+        """
         final_list.append(product)
 
     return final_list
